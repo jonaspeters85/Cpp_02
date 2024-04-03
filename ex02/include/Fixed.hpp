a1e527706569dc874afc9024362ef8c522e40b78
Jonas Peters <jopeters@c2r13s4.42wolfsburg.de>
2024-04-03 13:53:46 +0200

    cpp02 ex02

---
+++ ex02/include/Fixed.hpp
@@ -0,0 +1,53 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class Fixed
+{
+	protected:
+
+	private:
+		int fixedPointValue;
+		static const int fractBits = 8;
+
+	public:		
+		void	setRawBits(int const value);
+		int		getRawBits(void) const;
+
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+
+    	// Overloaded insertion operator as a member function
+    	friend std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+
+		// ****** ex03 ******
+		friend std::ostream& operator>(std::ostream& os, const Fixed& obj);
+
+
+
+
+		//Fixed(const std::string& str); 			// Constructor for std::cout << overloading
+
+		Fixed(void);
+		~Fixed(void);
+
+		Fixed(const int value);
+		Fixed(const float value);
+		
+
+		Fixed(const Fixed& other);				// Copy constructor
+		Fixed& operator=(const Fixed& other);	// Copy assignment operator
+
+};
+#endif
+
+
+
+
---
+++ ex02/src/Fixed.cpp
@@ -0,0 +1,86 @@
+#include "../include/Fixed.hpp" 
+
+Fixed::Fixed(void)
+    : fixedPointValue(0)
+{
+   	std::cout << "Default constructor called " << std::endl;
+}
+
+// Copy constructor
+Fixed::Fixed(const Fixed &other)
+{
+	(void)other;
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+// Copy assignment operator
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		this->setRawBits(other.getRawBits());
+
+	return *this;
+}
+
+Fixed::Fixed(const int value)
+{
+	(void) value;
+	std::cout << "Int constructor called " << value << std::endl;
+
+	this->setRawBits(value << fractBits);
+}
+
+Fixed::Fixed(const float value)
+{
+	(void) value;
+	std::cout << "Float constructor called " << value << std::endl;
+
+	this->fixedPointValue = static_cast<int>(roundf(value * (1 << fractBits)));
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& obj)
+{
+	os << obj.toFloat(); 
+	return os;
+}
+
+// ******* Member Functions **********
+
+void Fixed::setRawBits(int value)
+{
+	this->fixedPointValue = value;
+};
+
+int Fixed::getRawBits(void) const
+{
+	return (this->fixedPointValue);
+};
+
+// fixed point value to floating point value
+float Fixed::toFloat(void) const
+{
+	return static_cast<float>(this->getRawBits())/ (1 << fractBits);
+}
+
+// fixed point value to int
+int Fixed::toInt(void) const
+{
+	return (this->getRawBits() >> fractBits);
+}
+
+// ++++++++ New Operators +++++++++++
+std::ostream& operator>(std::ostream& os, const Fixed& obj)
+{
+	(void) obj;
+	std::cout << " HUHU" << std::endl;
+	return os;
+	//os << obj.toFloat(); 
+	//return os;
+}
